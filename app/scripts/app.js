6aa2690a6b1957d0e85c39ac3354b77c6169704a
jlbates <locke504@gmail.com>
2016-01-10 10:06:08 -0600

    First commit with non-tweaked imports from angular yeoman generator

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ns2StatsApp
+ * @description
+ * # ns2StatsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ns2StatsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ns2StatsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ns2StatsApp
+ */
+angular.module('ns2StatsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
